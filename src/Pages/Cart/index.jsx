fadf39aab96d1953dc32806ab664adfe992f0d4b
shivanandgupta <shivanandgupta316@gmail.com>
2025-08-06 16:00:05 +0530

    cartitem corrected

--- src/Pages/Cart/index.jsx
+++ src/Pages/Cart/index.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { IoBagCheck } from "react-icons/io5";
 import Button from '@mui/material/Button';
-import CartItem from './cartItems';
+import CartItem from './CartItems';
 
 export default function Cart() {
 
